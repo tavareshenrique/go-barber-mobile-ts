4c37335f607fb2d0d49ddaa9a9510777400235a4
Henrique Tavares <ihenrits@gmail.com>
2020-04-25 01:56:20 -0300

    Adicionando Babel Root Import no React Native

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+
+import Dashboard from '~/components/Dashboard';
 
 const App: React.FC = () => (
   <View>
-    <Text>GoBarber</Text>
+    <Dashboard />
   </View>
 );
 export default App;
