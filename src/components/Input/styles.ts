312b65510bd8cd4cfc8836aa5f888bc61c92737c
Henrique Tavares <ihenrits@gmail.com>
2020-04-26 13:04:50 -0300

    Foco e preenchimento

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,16 +1,27 @@
-import styled from 'styled-components/native';
+import styled, { css } from 'styled-components/native';
 import FeatherIcon from 'react-native-vector-icons/Feather';
 
-export const Container = styled.View`
+interface ContainerProps {
+  isFocused: boolean;
+}
+
+export const Container = styled.View<ContainerProps>`
   width: 100%;
   height: 60px;
   padding: 0 16px;
   background: #232129;
   border-radius: 10px;
+  border: 2px solid #232129;
   margin-bottom: 8px;
 
   flex-direction: row;
   align-items: center;
+
+  ${props =>
+    props.isFocused &&
+    css`
+      border-color: #ff9000;
+    `}
 `;
 
 export const TextInput = styled.TextInput`
