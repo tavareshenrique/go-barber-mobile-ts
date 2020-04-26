86afa8b59dfd8e3f73ca0ceb01461868ba1babaf
Henrique Tavares <ihenrits@gmail.com>
2020-04-26 00:16:00 -0300

    Input & Button

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Image } from 'react-native';
 
+import Input from '~/components/Input';
+import Button from '~/components/Button';
+
 import logoImg from '~/assets/logo.png';
 
 import { Container, Title } from './styles';
@@ -11,6 +14,18 @@ const SignIn: React.FC = () => {
       <Image source={logoImg} />
 
       <Title>Faça seu logon</Title>
+
+      <Input name="email" icon="mail" placeholder="E-mail" />
+
+      <Input name="passowrd" icon="lock" placeholder="Senha" />
+
+      <Button
+        onPress={() => {
+          console.log('Button');
+        }}
+      >
+        Entrar
+      </Button>
     </Container>
   );
 };
