16ca49d0bb361429257c0bfd38037ceee1af9f07
Henrique Tavares <ihenrits@gmail.com>
2020-06-13 15:10:37 -0300

    Estrutura da criaçao

--- src/pages/CreateAppointment/index.tsx
+++ src/pages/CreateAppointment/index.tsx
@@ -1,10 +1,43 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useCallback } from 'react';
+import { useRoute, useNavigation } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/Feather';
+import { useAuth } from '~/hooks/auth';
 
-const CreateAppointment: React.FC = () => (
-  <View>
-    <Text>CreateAppointment</Text>
-  </View>
-);
+import { RouteParams } from './interfaces';
+
+import {
+  Container,
+  Header,
+  BackButton,
+  HeaderTitle,
+  UserAvatar,
+} from './styles';
+
+const CreateAppointment: React.FC = () => {
+  const route = useRoute();
+  const { providerId } = route.params as RouteParams;
+
+  const { goBack } = useNavigation();
+
+  const { user } = useAuth();
+
+  const handleNavigateBack = useCallback(() => {
+    goBack();
+  }, [goBack]);
+
+  return (
+    <Container>
+      <Header>
+        <BackButton onPress={handleNavigateBack}>
+          <Icon name="chevron-left" size={24} color="#999591" />
+        </BackButton>
+
+        <HeaderTitle>Cabeleireiros</HeaderTitle>
+
+        <UserAvatar source={{ uri: user.avatar_url }} />
+      </Header>
+    </Container>
+  );
+};
 
 export default CreateAppointment;
